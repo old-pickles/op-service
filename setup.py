46760afd2eaa9ee8fdad7a1e5161ffbe44938111
Alexander Exarhos <Alex@Alexs-15-MacBook-Pro.local>
2016-12-19 07:42:45 -0800

    Updated .gitignore

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import sys
 assert sys.version_info >= (2, 7), 'Requires Python 2.7+'
 
 setuptools.setup(
-    name='op_service',
-    version='1.0.0',
-    packages=['op_service'],
-    author='Alex Exarhos',
-    author_email='alexexarhos@gmail.com'
+  name='op_service',
+  version='1.0.0',
+  packages=['op_service'],
+  author='Alex Exarhos',
+  author_email='alexexarhos@gmail.com'
 )
