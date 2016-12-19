750841c18d0ba97dfb0fe06c290000d3225a5d71
Alexander Exarhos <Alex@Alexs-15-MacBook-Pro.local>
2016-12-19 07:52:25 -0800

    Replaced requirements.txt with install_requires in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ assert sys.version_info >= (2, 7), 'Requires Python 2.7+'
 setuptools.setup(
   name='op_service',
   version='1.0.0',
-  packages=['op_service'],
   author='Alex Exarhos',
-  author_email='alexexarhos@gmail.com'
+  author_email='alexexarhos@gmail.com',
+  packages=[
+    'op_service'
+  ],
+  install_requires=[
+    'flask >= 0.11.1',
+    'flask-cors >= 3.0.2'
+  ]
 )
